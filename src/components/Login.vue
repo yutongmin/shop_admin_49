32166313946bbf065f3999d9b1840d59f7725053
yutongmin <17719316036@163.com>
2019-10-15 20:30:35 +0800

    登录页面功能全部完成--登录拦截完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="login">
-    <el-form :rules="rules" status-icon ref="form" :model="form" label-width="80px">
-      <img class="logo" src="../assets/avatar.jpg" alt />
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+    <el-form :rules="rules"
+             status-icon
+             ref="form"
+             :model="form"
+             label-width="80px">
+      <img class="logo"
+           src="../assets/avatar.jpg"
+           alt />
+      <el-form-item label="用户名"
+                    prop="username">
+        <el-input v-model="form.username"
+                  placeholder="请输入用户名"></el-input>
       </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
+      <el-form-item label="密码"
+                    prop="password">
+        <el-input v-model="form.password"
+                  placeholder="请输入密码"
+                  type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button @click="login" class="loginBtn" type="primary">登录</el-button>
+        <el-button @click="login"
+                   class="loginBtn"
+                   type="primary">登录</el-button>
         <el-button @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
@@ -65,10 +78,11 @@ export default {
 
         // 校验成功,发送ajax请求
         axios
-          .post('http://localhost:8888/api//v1/login', this.form)
+          .post('http://localhost:8888/api/private/v1/login', this.form)
           .then(res => {
-            const { meta } = res.data
+            const { meta, data } = res.data
             if (meta.status === 200) {
+              localStorage.setItem('token', data.token)
               this.$message({
                 type: 'success',
                 message: meta.msg,
--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Index from './components/Index.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [{
     path: '/',
     redirect: '/index'
@@ -19,3 +19,20 @@ export default new Router({
     component: Index
   }]
 })
+// 给路由对象注册导航守卫, 守卫导航
+// to: 到哪去
+// from: 从哪来
+// next()  next() 放行,  next('/login')
+router.beforeEach((to, from, next) => {
+  // 所有的导航的跳转都要经过导航守卫
+  // 关注to, 如果to表示去登陆, 直接放行
+  // 如果to不是去登陆, 去其他组件, 判断是否有token, 有就放行, 没有就去登陆
+  const token = localStorage.getItem('token')
+  if (to.path === '/login' || token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+
+export default router
